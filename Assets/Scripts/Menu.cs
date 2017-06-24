5cae1338dbf36851bf304db6d888d27f0ba19e22
Brad <bachippi@svsu.edu>
2017-06-24 12:40:26 -0400

    rubby sucks

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public Canvas MainCanvas;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
